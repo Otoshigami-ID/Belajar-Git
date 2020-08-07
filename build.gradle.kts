4eec9191af80ae46cd0deedd1a2f5fd1c715d669
Rifqi Maheswara <rifqimaheswara@gmail.com>
2020-08-07 09:32:05 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.stematel.rifqi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,10 @@
+/*
+Nama  : Rifky Thara Basuki
+Kelas : XI RPL 2
+No    : 25
+ */
+
+fun main(){
+    println("Hello Kotlin")
+}
+
